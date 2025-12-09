38bf25c4379e52d9d0c49f1730c7d7070371f3e6
Taras <tarasostapchuk27@gmail.com>
2025-12-09 12:16:41 +0100

    Make urls

--- config/urls.py
+++ config/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('todo/', include('todo_list.urls')),
+    path('', include('todo_list.urls')),
 ]
--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,6 +1,20 @@
 from django.urls import path
-from . import views
+from .views import (
+    TaskListView, TaskCreateView, TaskUpdateView, TaskDeleteView, ToggleCompleteView,
+    TagListView, TagCreateView, TagUpdateView, TagDeleteView
+)
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("", TaskListView.as_view(), name="task-list"),
+    path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("tasks/<int:pk>/toggle/", ToggleCompleteView.as_view(), name="task-toggle"),
+
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/add/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
 ]
+
+app_name = "todo_list"
